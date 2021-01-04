53500d12365c274083fa1d0b683457ba21765a00
Vosters <mike.r.vosters@gmail.com>
2021-01-04 12:55:48 -0600

    Fixed AuthScreen headline change bug. Fixed missing H3 error on ContentScreen in AvyName.

--- packages/mobile/android/app/src/main/java/com/wellemental/wellemental/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/wellemental/wellemental/MainActivity.java
@@ -1,4 +1,4 @@
-package com.wellemental.wellemental;
+package com.wellemental;
 
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
--- packages/mobile/android/app/src/main/java/com/wellemental/wellemental/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wellemental/wellemental/MainApplication.java
@@ -1,4 +1,4 @@
-package com.wellemental.wellemental;
+package com.wellemental;
 
 import android.app.Application;
 import android.content.Context;
@@ -31,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected String getJSMainModuleName() {
-          return "index";
+          return "packages/mobile/index";
         }
       };
 
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.wellemental.wellemental.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.wellemental.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
